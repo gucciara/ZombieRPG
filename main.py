0ed9ffd3cc0f4c1951f26c8451a000b494b8626d
zachmeitz <158590983+zachmeitz@users.noreply.github.com>
2024-03-12 12:26:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,60 @@
-
-
-from sprites import *
-from configuration import *
 import pygame
+from sprites import *
+from config import *
 import sys
-
-class Spritesheet:
-    def __init__(self, path):
-        self.spritesheet = pygame.image.load(path).convert()
-
-    def get_image(self, x, y, width, height):
-        sprite = pygame.Surface([width, height])
-        sprite.blit(self.spritesheet, (0,0), (x,y,width,height))
-        sprite.set_colorkey(BLACK)
-
-        return sprite
 
 class Game:
 
-
     def __init__(self):
+        pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.terrain_spritesheet = Spritesheet('/Users/Tone/PycharmProjects/ZombieRPG/tiles_spritesheet.png')
         self.running = True
 
-    def createTileMap(self):
-        for i, row in enumerate(tilemap):
-            for j, column in enumerate(row):
-                Ground(self,j,i)
-                if column=='B':
-                    Block(self,j,i)
+    def new(self):
+        # a new game starts
+        self.playing = True
 
-    #create sprites
-    def create(self):
-        self.all_sprites=pygame.sprite.LayeredUpdates()
-        self.createTileMap()
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
 
-    #update world and each sprite each frame
-    def update(self):
-        self.all_sprites.update()
-    #handles all button presses and causes the game to respond as desired
+        self.player = Player(self, 1, 2)
+
     def events(self):
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running=False
+                self.playing = False
+                self.running = False
+    def update(self):
+        self.all_sprites.update()
 
-    #draws sprites every single frame
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
         self.clock.tick(FPS)
         pygame.display.update()
-    #handles everything within the game while it is going
+
     def main(self):
-        while self.running:
+        while self.playing:
             self.events()
             self.update()
             self.draw()
+        self.running = False
 
+    def game_over(self):
+        pass
+    def intro_screen(self):
+        pass
 
-game = Game()
-game.create()
-
-while game.running:
-    game.main()
-
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
 
 pygame.quit()
 sys.exit()
--- sprites.py
+++ sprites.py
@@ -1,41 +1,54 @@
-from configuration import *
 import pygame
+from config import *
+import math
+import random
 
-#use this as a template for every class. You will be replacing the x and y coordinate in coordination w/ spritesheet
-class Block(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
-
-        self.game=game
-        self._layer=BLOCKS_LAYER
+        self.game = game
+        self._layer = PLAYER_LAYER
         self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
 
-        self.x = x*TILESIZE
-        self.y = y*TILESIZE
-
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
         self.width = TILESIZE
         self.height = TILESIZE
 
-        self.image = self.game.terrain_spritesheet.get_image(31,0, self.width, self.height)
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+
+        image_to_load = pygame.image.load("img/character.png")
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.set_colorkey(BLACK)
+        self.image.blit(image_to_load, (0,0))
+
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self, game, x, y):
+    def update(self):
+        self.movement()
 
-        self.game=game
-        self._layer=GROUND_LAYER
-        self.groups = self.game.all_sprites
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
 
-        self.x = x*TILESIZE
-        self.y = y*TILESIZE
+        self.x_change = 0
+        self.y_change = 0
 
-        self.width = TILESIZE
-        self.height = TILESIZE
-
-        self.image = self.game.terrain_spritesheet.get_image(0,0, self.width, self.height)
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
