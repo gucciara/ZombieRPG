d711f4ad5ba6a9904c4b6fc1ea084a5d180318c7
gucciara <143750042+gucciara@users.noreply.github.com>
2024-03-12 12:50:20 -0400

    Update main.py
    we're functional! Draws a map, adds a character, can move that character around

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import pygame
 from sprites import *
-from config import *
+from configuration import *
+import pygame
 import sys
+
+class Spritesheet:
+    def __init__(self, path):
+        self.spritesheet = pygame.image.load(path).convert()
+
+    def get_image(self, x, y, width, height):
+        sprite = pygame.Surface([width, height])
+        sprite.blit(self.spritesheet, (0,0), (x,y,width,height))
+        sprite.set_colorkey(BLACK)
+
+        return sprite
 
 class Game:
 
@@ -9,52 +20,52 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.terrain_spritesheet = Spritesheet('/Users/Tone/PycharmProjects/ZombieRPG/tiles_spritesheet.png')
         self.running = True
 
-    def new(self):
-        # a new game starts
-        self.playing = True
+    def createTileMap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                Ground(self, j, i)
+                if column == 'B':
+                    Block(self, j, i)
 
+    def create(self):
         self.all_sprites = pygame.sprite.LayeredUpdates()
-        self.blocks = pygame.sprite.LayeredUpdates()
-        self.enemies = pygame.sprite.LayeredUpdates()
-        self.attacks = pygame.sprite.LayeredUpdates()
-
+        self.createTileMap()
         self.player = Player(self, 1, 2)
 
     def events(self):
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.playing = False
                 self.running = False
+            # Pass player events to player class
+            self.player.handle_event(event)
+
     def update(self):
         self.all_sprites.update()
+        # Update player
+        self.player.update()
 
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
+        # Draw player
+        self.screen.blit(self.player.image, self.player.rect)
         self.clock.tick(FPS)
         pygame.display.update()
 
     def main(self):
-        while self.playing:
+        while self.running:
             self.events()
             self.update()
             self.draw()
-        self.running = False
 
-    def game_over(self):
-        pass
-    def intro_screen(self):
-        pass
+game = Game()
+game.create()
 
-g = Game()
-g.intro_screen()
-g.new()
-while g.running:
-    g.main()
-    g.game_over()
+while game.running:
+    game.main()
 
 pygame.quit()
 sys.exit()
